9d9d3a794e1a8e26c17b7f8f7ba93b0041504f83
muaazsalagar <salagarmi@gmail.com>
2016-01-16 14:38:04 -0500

    Home page updates

--- server.js
+++ server.js
@@ -2,9 +2,15 @@ var express = require('express');
 var app = express();
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+
 app.get('/hello', function(req, res){
     res.send('hello world');
 });
+
+app.get('/', function(req, res){
+    res.send('THIS PAGE IS UNDER CONSTRUCTION');
+});
+
 
 
 app.listen(port, ipaddress);
