8e871b12a9feaef6d9890e06e60018821c100b18
muaazsalagar <salagarmi@gmail.com>
2016-01-21 16:52:22 -0500

    changing the index file updated with personal website theme and home page

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public/personalwebsite'));
 
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
