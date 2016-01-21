6da12811ae8b9365ec1e642eb0b92cda207876aa
muaazsalagar <salagarmi@gmail.com>
2016-01-20 19:22:42 -0500

    server file updated for public directory access

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var app = express();
+app.use(express.static(__dirname + '/public'));
+
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
 
