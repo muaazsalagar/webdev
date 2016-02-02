28f79b5a8a28bc7c37b8d322a4929c86bcd9be14
muaazsalagar <salagarmi@gmail.com>
2016-02-01 23:35:24 -0500

    Home Page done!

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var app = express();
 app.use(express.static(__dirname + '/public/personalwebsite'));
+app.use(express.static(__dirname + '/public'));
+
+
 
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
