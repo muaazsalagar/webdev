3e6b963c8f32ca5693ce36d9da7273d2592da7e6
muaazsalagar <salagarmi@gmail.com>
2016-02-04 23:59:54 -0500

    form field done

---
+++ public/assignment/js/redirections.js
@@ -0,0 +1,26 @@
+/**
+ * Created by muaazsalagar on 2/4/16.
+ */
+
+
+$('#registerButton').click(function() {
+    window.location.href = 'profile.html';
+    return false;
+});
+
+
+$('#loginButton').click(function() {
+    window.location.href = 'profile.html';
+    return false;
+});
+
+$('#formFields').click(function() {
+    window.location.href = 'form-fields.html';
+    return false;
+});
+
+
+
+
+
+
