26e9230d770dee4d629d7d7d858dc65c7ba205ff
muaazsalagar <salagarmi@gmail.com>
2016-02-17 18:54:45 -0500

    ng-route location example

---
+++ public/experiments/angularjs/ng-route-controllers/app.js
@@ -0,0 +1,39 @@
+/**
+ * Created by muaazsalagar on 2/17/16.
+ */
+
+
+
+(function(){
+    angular
+        .module("WhiteBoardApp", ["ngRoute"])
+        .config(function($routeProvider){
+
+            // we are configuring the route provider which is service provided by angular framerwork
+
+
+            // route provider is the service by
+            $routeProvider
+                .when("/", {
+                    templateUrl: "home.html"
+                })
+                .when("/profile", {
+                    templateUrl: "profile.html"
+                })
+                .when("/admin", {
+                    templateUrl: "admin.html"
+                })
+                .otherwise({
+                    redirectTo: "/"
+                });
+        }).controller("NavController", function($scope, $location) {
+        $scope.$location = $location;
+
+        // here we can change scope and location param values ; parameter order does not matter
+        // this is called dependecny injection same as Spring MVC and other framerwork
+
+        // problem or flaw with this is : production deployemtn uses minified version function param names are
+        // shortended so we need to work around that
+    });
+
+})();
