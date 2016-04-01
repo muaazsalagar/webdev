b1fa204e5ab4057fd06560119fb59e68a976f6d5
muaazsalagar <salagarmi@gmail.com>
2016-04-01 01:21:16 -0400

    home page search fixed

---
+++ public/project/client/services/admin.service.client.js
@@ -0,0 +1,139 @@
+/**
+ * Created by muaazsalagar on 2/03/12.
+ */
+
+(function () {
+    angular
+        .module("BanquetApp")
+        .factory("UserService", UserService);
+
+    function UserService($http, $q, $rootScope) {
+
+        var api = {
+            // declaration of methods by following standards of  papa john
+
+
+            createUser:createUser,
+            setCurrentUser:setCurrentUser,
+            getCurrentUser:getCurrentUser,
+
+            // Api
+            findUserByCredentials:findUserByCredentials,
+            findUserByUsername:findUserByUsername,
+            findAllUsers:findAllUsers,
+            updateUserByID:updateUserByID,
+            findUserByID:findUserByID,
+            deleteUserById:deleteUserById
+        };
+
+        return api;
+
+
+        function findUserByCredentials(username, password) {
+
+            var deferred = $q.defer();
+
+            var url = "/api/assignment/user?username=:username&password=:password";
+            url = url.replace(":username", username);
+            url = url.replace(":password", password);
+
+            $http.get(url).success (function (response) {
+                deferred.resolve(response);
+            });
+
+            return deferred.promise;
+        }
+
+        function createUser(user)
+        {
+            var deferred=$q.defer();
+            var url="/api/assignment/user";
+            $http.post(url,user).success(function(response){
+                deferred.resolve(response);
+            });
+
+            return deferred.promise;
+
+
+        }
+
+        function setCurrentUser (user) {
+            $rootScope.currentUser = user;
+        }
+
+        function getCurrentUser () {
+            return $rootScope.currentUser;
+        }
+
+        function findUserByUsername(username)
+        {
+            var deferred=$q.defer();
+            var url="/api/assignment/user?username=username";
+            url=url.replace(":username",username);
+            $http.get(url).success(function(response){
+                deferred.resolve(response);
+            });
+
+            return deferred.promise;
+
+
+        }
+
+        function findAllUsers(){
+            var deferred=$q.defer();
+            var url="/api/assignment/user";
+
+            $http.get(url).success(function(response){
+                deferred.resolve(response);
+            });
+
+
+            return deferred.promise;
+        }
+
+        function updateUserByID(userId,user)
+        {
+            var deferred=$q.defer();
+            var url="/api/assignment/user/:id";
+            url=url.replace(":id",userId);
+            console.log(url);
+            console.log(userId);
+
+            $http.put(url,user).success(function(response){
+                deferred.resolve(response);
+            });
+
+            return deferred.promise;
+        }
+
+        function deleteUserById(user)
+        {
+            var deferred=$q.defer();
+            var url="/api/assignment/user/:id";
+            $http.delete(url).success(function(response){
+                deferred.resolve(response);
+            });
+
+            return deferred.promise;
+        }
+
+        function findUserByID(userId)
+        {
+            console.log("Client Calling the finduserID to the server");
+            var deferred=$q.defer();
+            var url="/api/assignment/user/:id";
+            url=url.replace(":id",userId);
+            console.log(url);
+            console.log(userId);
+
+            $http.get(url).success(function(response){
+                deferred.resolve(response);
+            });
+
+            return deferred.promise;
+
+        }
+
+
+    }
+})();
