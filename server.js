c8f8e57fe3a56fe7d6e058c67a460ddcaa0b47df
muaazsalagar <salagarmi@gmail.com>
2016-04-01 22:16:30 -0400

    pages fixed

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
 
 require("./public/assignment/server/app.js")(app, uuid);
-require("./public/platform9Assignment/server/app.js")(app, uuid);
+require("./public/ColorAPIExperiment/server/app.js")(app, uuid);
 require("./public/project/server/app.js")(app, uuid);
 
 
